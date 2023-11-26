ec63e4d1e59a14766982cfcff37efed5d8596fb9
DriftSolutions <57127451+DriftSolutions@users.noreply.github.com>
2023-11-26 00:16:54 -0500

    Cleanup SendQ

--- v5/src/SendQ.cpp
+++ v5/src/SendQ.cpp
@@ -25,48 +25,87 @@
 #if defined(IRCBOT_ENABLE_IRC)
 #include "SendQ.h"
 #include <memory>
-#define SENDQ_MEMLEAK
 
-/** \struct SENDQ_QUEUE
- * An entry in the Send Queue (rate-limited data sender, to keep the bot from flooding off servers)
- */
-struct SENDQ_QUEUE {
-	T_SOCKET * sock; ///< The socket to send data to.
-	char * data; ///< The data to send
-	int netno;
-	uint8 priority; ///< The send priority
-	int32 len; ///< The length of data to send
-	time_t sendAt;
-};
-
-#define CLEANUP_AFTER 10
-class QueueBuffer {
-public:
-	DSL_BUFFER buffer;
-	int ref_cnt;
-	int netno;
-
-	QueueBuffer() {
-		buffer_init(&buffer, false);
-		ref_cnt = CLEANUP_AFTER;
-		netno = -1;
-	}
-	~QueueBuffer() {
-		buffer_free(&buffer);
-	}
-};
-
-typedef std::vector<SENDQ_QUEUE *> SendQueueType;
-typedef std::map<T_SOCKET *, SendQueueType> PriorityQueueType;
-
-// larger queue
 Titus_Mutex sqMutex;
-PriorityQueueType PriorityQueue;
-// immediate buffers
-std::map<T_SOCKET *, shared_ptr<QueueBuffer>> buffers;
 
 SENDQ_STATS sq_stats;
 SENDQ_STATS * SendQ_GetStats() { return &sq_stats; }
+
+/** \struct SENDQ_ENTRY
+ * An entry in the Send Queue (rate-limited data sender, to keep the bot from flooding off servers)
+ */
+class SENDQ_ENTRY {
+public:
+	time_t sendAt; ///< Don't send the data before this timestamp
+	string data; ///< The data to send
+};
+
+class SendQueue {
+public:
+	DSL_BUFFER buffer;
+	int netno = -1;
+	time_t lastUsed = time(NULL);
+	map<uint8, vector<SENDQ_ENTRY>, greater<uint8>> queue;
+
+	void Queue(uint8 priority, int delay, const string& data) {
+		AutoMutex(sqMutex);
+		if (queue.find(priority) == queue.end()) {
+			queue.insert({ priority, {} });
+		}
+		queue[priority].push_back({ delay + time(NULL), std::move(data) });
+		sq_stats.queued_items++;
+		lastUsed = time(NULL);
+	}
+	void FillBuffer() {
+		// Fill 1 second of the highest priority data to send
+		time_t tme = time(NULL);
+		AutoMutex(sqMutex);
+		if (buffer.len < config.base.sendq) {
+			for (auto p = queue.begin(); p != queue.end() && buffer.len < config.base.sendq; p++) {
+				for (auto x = p->second.begin(); x != p->second.end() && buffer.len < config.base.sendq;) {
+					if (x->sendAt <= tme) {
+#ifdef DEBUG
+						if (netno >= 0) {
+							ib_printf("SendQ[%p]: IRC[%d]: Added %zu bytes: %s", this, netno, x->data.length(), x->data.c_str());
+						} else {
+							ib_printf("SendQ[%p]: non-IRC: Added %zu bytes: %s", this, x->data.length(), x->data.c_str());
+						}
+#endif
+						buffer_append(&buffer, x->data.c_str(), x->data.length());
+						sq_stats.queued_items--;
+						lastUsed = time(NULL);
+						x = p->second.erase(x);
+					} else {
+						x++;
+					}
+				}
+			}
+		}
+	}
+	bool IsIdle() {
+		AutoMutex(sqMutex);
+		return ((time(NULL) - lastUsed) > 60);
+	}
+	bool IsEmpty() {
+		AutoMutex(sqMutex);
+		return (buffer.len == 0 && queue.size() == 0);
+	}
+
+	SendQueue() {
+		AutoMutex(sqMutex);
+		buffer_init(&buffer, false);
+	}
+	~SendQueue() {
+		AutoMutex(sqMutex);
+		buffer_free(&buffer);
+		for (auto& p : queue) {
+			sq_stats.queued_items -= p.second.size();
+		}
+		queue.clear();
+	}
+};
+typedef map<T_SOCKET *, shared_ptr<SendQueue>> PriorityQueueType;
+PriorityQueueType PriorityQueue;
 
 /*
 void Push_History(uint32 level, uint32 val) {
@@ -98,104 +137,38 @@ void Push_History(uint32 level, uint32 val) {
  */
 int bSend(T_SOCKET * sock, const char * buf, int32 len, uint8 priority, uint32 delay) {
 	if (len <= 0) { len = strlen(buf); }
-#if defined(SENDQ_MEMLEAK)
-	SENDQ_QUEUE * Scan = znew(SENDQ_QUEUE);
-#else
-	SENDQ_QUEUE * Scan = new SENDQ_QUEUE;
-#endif
-	memset(Scan, 0, sizeof(SENDQ_QUEUE));
-	Scan->netno = -1;
-	Scan->len = len;
-	Scan->priority = priority;
-	Scan->sock = sock;
-	Scan->sendAt = time(NULL) + delay;
-#if defined(SENDQ_MEMLEAK)
-	Scan->data = (char *)zmalloc(len + 1);
-#else
-	Scan->data = new char[len + 1];
-#endif
-	Scan->data[len] = 0;
-	memcpy(Scan->data, buf, len);
-
+	
 	AutoMutex(sqMutex);
+	shared_ptr<SendQueue> q;
+	auto x = PriorityQueue.find(sock);
+	if (x != PriorityQueue.end()) {
+		q = x->second;
+	} else {
+		q = make_shared<SendQueue>();
+		PriorityQueue.insert({ sock, q });
+	}
+
+	string data;
+	data.assign(buf, len);
 #if defined(IRCBOT_ENABLE_IRC)
-	for (int i=0; i < config.num_irc; i++) {
-		if (config.ircnets[i].sock == sock) {
-			Scan->netno = i;
-			if (config.ircnets[i].log_fp) {
-				char durbuf[32];
-				fprintf(config.ircnets[i].log_fp, "%s > %s", ircbot_cycles_ts(durbuf,sizeof(durbuf)), buf);
-				fflush(config.ircnets[i].log_fp);
+	if (q->netno == -1) {
+		for (int i = 0; i < config.num_irc; i++) {
+			if (config.ircnets[i].sock == sock) {
+				q->netno = i;
+				break;
 			}
-			break;
 		}
 	}
+	if (q->netno >= 0 && config.ircnets[q->netno].sock == sock && config.ircnets[q->netno].log_fp != NULL) {
+		char durbuf[32];
+		fprintf(config.ircnets[q->netno].log_fp, "%s > %s", ircbot_cycles_ts(durbuf, sizeof(durbuf)), data.c_str());
+		fflush(config.ircnets[q->netno].log_fp);
+	}
 #endif
-	sq_stats.queued_items++;
-
-	PriorityQueue[sock].push_back(Scan);
+	q->Queue(priority, delay, data);
 
 	return len;
 }
-
-/**
- * Deletes a queue entry (data and structure).
- * @param q the queue entry to free
- */
-void SendQ_Delete(SENDQ_QUEUE * q) {
-	AutoMutex(sqMutex);
-#if defined(SENDQ_MEMLEAK)
-	zfree(q->data);
-	zfree(q);
-#else
-	delete [] q->data;
-	delete q;
-#endif
-}
-
-/**
- * Deletes a queue entry from PriorityQueue and optionally deletes the data and structure.
- * @param q the queue entry to free
- */
-/*
-void SendQ_RemoveFromQueue(SENDQ_QUEUE * q, bool also_delete=false) {
-	sqMutex.Lock();
-
-	auto z = PriorityQueue.find(q->sock);
-	if (z != PriorityQueue.end()) {
-		for (auto y = z->second.queue.begin(); y != z->second.queue.end(); y++) {
-			if (*y == q) {
-				if (also_delete) {
-					SendQ_Delete(q);
-				}
-				z->second.queue.erase(y);
-				if (z->second.buffer.len == 0 && z->second.queue.size() == 0) { PriorityQueue.erase(z); }
-				sq_stats.queued_items--;
-				sqMutex.Release();
-				return;
-			}
-		}
-	}
-
-	// the pointer should never be found in this search, but I do it anyway just in case
-	for (auto x = PriorityQueue.begin(); x != PriorityQueue.end(); x++) {
-		for (auto y = x->second.queue.begin(); y != x->second.queue.end(); y++) {
-			if (*y == q) {
-				if (also_delete) {
-					SendQ_Delete(q);
-				}
-				x->second.queue.erase(y);
-				if (x->second.buffer.len == 0 && x->second.queue.size() == 0) { PriorityQueue.erase(x); }
-				sq_stats.queued_items--;
-				sqMutex.Release();
-				return;
-			}
-		}
-	}
-
-	sqMutex.Release();
-}
-*/
 
 /**
  * Deletes all queue entries that are using a certain socket. This is useful for cases where the socket gets an error or is closed remotely and you need to clear out the SendQ of it's entries
@@ -205,32 +178,17 @@ void SendQ_ClearSockEntries(T_SOCKET * sock) {
 	AutoMutex(sqMutex);
 	auto z = PriorityQueue.find(sock);
 	if (z != PriorityQueue.end()) {
-		for (auto y = z->second.begin(); y != z->second.end(); y++) {
-			SendQ_Delete(*y);
-		}
-		z->second.clear();
 		PriorityQueue.erase(z);
-	}
-
-	auto x = buffers.find(sock);
-	if (x != buffers.end()) {
-		buffer_clear(&x->second->buffer);
-		x->second->ref_cnt = 0;
 	}
 }
 
 bool SendQ_HaveQueueItems() {
 	AutoMutex(sqMutex);
-	for (auto x = PriorityQueue.begin(); x != PriorityQueue.end(); x++) {
-		if (x->second.size() > 0) {
+	for (auto& x : PriorityQueue) {
+		if (!x.second->IsEmpty()) {
 			return true;
 		}
 	}
-
-	if (buffers.size()) {
-		return true;
-	}
-
 	return false;
 }
 
@@ -240,68 +198,11 @@ bool SendQ_HaveQueueItems() {
 */
 bool SendQ_HaveSockEntries(T_SOCKET * sock) {
 	AutoMutex(sqMutex);
-	auto z = PriorityQueue.find(sock);
-	if (z != PriorityQueue.end()) {
-		if (z->second.size()) {
-			return true;
-		}
-	}
-
-	auto x = buffers.find(sock);
-	if (x != buffers.end()) {
-		return true;
-	}
-
-	return false;
-}
-
-/**
- * Returns the number of SendQ queue entries that are for a certain socket.
- * @param sock the socket to match in the queue entries
- */
-uint32 SendQ_CountSockEntries(T_SOCKET * sock) {
-	uint32 ret = 0;
-	AutoMutex(sqMutex);
-	auto z = PriorityQueue.find(sock);
-	if (z != PriorityQueue.end()) {
-		ret += z->second.size();
-	}
-
-	auto x = buffers.find(sock);
-	if (x != buffers.end()) {
-		ret++;
-	}
-
-	return ret;
-}
-
-/**
- * Grabs the queue entry from the SendQ that has the highest priority to send.
- */
-SENDQ_QUEUE * SendQ_GetNextEntry(T_SOCKET * sock, bool also_remove) {
-	AutoMutex(sqMutex);
-	time_t tme = time(NULL);
 	auto x = PriorityQueue.find(sock);
 	if (x != PriorityQueue.end()) {
-		auto highest = x->second.begin();
-		for (auto y = x->second.begin(); y != x->second.end(); y++) {
-			if ((*y)->sendAt <= tme && (*y)->priority > (*highest)->priority) {
-				highest = y;
-			}
-		}
-		if (highest != x->second.end()) {
-			SENDQ_QUEUE * ret = *highest;
-			if (also_remove) {
-				x->second.erase(highest);
-				if (x->second.size() == 0) {
-					PriorityQueue.erase(x);
-				}
-				sq_stats.queued_items--;
-			}
-			return ret;
-		}
+		return !x->second->IsEmpty();
 	}
-	return NULL;
+	return false;
 }
 
 /**
@@ -326,83 +227,46 @@ THREADTYPE SendQ_Thread(void * lpData) {
 		time_t cur = time(NULL);
 		if (cur >= nextSend) {
 			AutoMutex(sqMutex);
-			// make sure there is a buffer for all queued items
-			for (auto curque = PriorityQueue.begin(); curque != PriorityQueue.end(); curque++) {
-				auto curbuf = buffers.find(curque->first);
-				if (curbuf == buffers.end()) {
-#ifdef DEBUG
-					ib_printf("SendQ[%p]: Creating buffer...\n", curque->first);
-#endif
-					buffers[curque->first] = make_shared<QueueBuffer>();
-				}
-			}
 
-			for (auto cursock = buffers.begin(); cursock != buffers.end(); cursock++) {
+			for (auto cursock = PriorityQueue.begin(); cursock != PriorityQueue.end();) {
 				if (!config.sockets->IsKnownSocket(cursock->first)) {
 					ib_printf("SendQ[%p]: Deleting entries for unknown socket...\n", cursock->first);
-					SendQ_ClearSockEntries(cursock->first);
-					//buffer_clear(&cursock->second.buffer);
-					continue;
-				}
-
-				while (cursock->second->buffer.len < config.base.sendq) {
-					SENDQ_QUEUE * Scan = SendQ_GetNextEntry(cursock->first, true);
-					if (Scan == NULL) {
-						// no more data for this socket, quit looking
-						break;
-					}
-
-					cursock->second->netno = Scan->netno;
-					buffer_append(&cursock->second->buffer, Scan->data, Scan->len);
+					cursock = PriorityQueue.erase(cursock);
+				} else if ((cursock->second->IsIdle() || config.shutdown_sendq) && cursock->second->IsEmpty()) {
 #ifdef DEBUG
-					if (Scan->netno >= 0) {
-						ib_printf("SendQ[%p]: IRC[%d]: Added %d bytes: %s", cursock->first, Scan->netno, Scan->len, Scan->data);
-					} else {
-						ib_printf("SendQ[%p]: non-IRC: Added %d bytes: %s\n", cursock->first, Scan->len, Scan->data);
-					}
+					ib_printf("SendQ[%p]: Deleting buffer...\n", cursock->first);
 #endif
-					SendQ_Delete(Scan);
-				}
-
-				if (cursock->second->buffer.len > 0) {
-					cursock->second->ref_cnt = CLEANUP_AFTER;
-					if (config.sockets->IsKnownSocket(cursock->first) && config.sockets->Select_Write(cursock->first, uint32(0)) == 1) {
-						int toSend = config.base.sendq;
-						if (cursock->second->buffer.len < toSend) { toSend = cursock->second->buffer.len; }
-#ifdef DEBUG
-						printf("SendQ[%p]: Sending %d bytes of " I64FMT "...\n", cursock->first, toSend, cursock->second->buffer.len);
-#endif
-						toSend = config.sockets->Send(cursock->first, cursock->second->buffer.data, toSend, false);
-						if (toSend <= 0) {
-							ib_printf("SendQ[%p]: Send returned %d\n", cursock->first, toSend);
-							SendQ_ClearSockEntries(cursock->first);
-							//buffer_clear(&cursock->second.buffer);
-							continue;
-						}
-
-						if (cursock->second->netno >= 0) {
-							sq_stats.bytesSentIRC += toSend;
-						} else {
-							sq_stats.bytesSent += toSend;
-						}
-
-						buffer_remove_front(&cursock->second->buffer, toSend);
-
-#ifdef DEBUG
-						printf("SendQ[%p]: Sent %d bytes.\n", cursock->first, toSend);
-#endif
-					}
-				}
-			}
-
-			for (auto x = buffers.begin(); x != buffers.end();) {
-				if (x->second->buffer.len == 0 && (config.shutdown_sendq || x->second->ref_cnt-- <= 0)) {
-#ifdef DEBUG
-					ib_printf("SendQ[%p]: Deleting buffer...\n", x->first);
-#endif
-					buffers.erase(x++);
+					cursock = PriorityQueue.erase(cursock);
 				} else {
-					x++;
+					cursock->second->FillBuffer();
+					if (cursock->second->buffer.len > 0) {
+						if (config.sockets->Select_Write(cursock->first, uint32(0)) == 1) {
+							int toSend = config.base.sendq;
+							if (cursock->second->buffer.len < toSend) { toSend = cursock->second->buffer.len; }
+#ifdef DEBUG
+							printf("SendQ[%p]: Sending %d bytes of " I64FMT "...\n", cursock->first, toSend, cursock->second->buffer.len);
+#endif
+							toSend = config.sockets->Send(cursock->first, cursock->second->buffer.data, toSend, false);
+							if (toSend <= 0) {
+								ib_printf("SendQ[%p]: Send returned %d\n", cursock->first, toSend);
+								cursock = PriorityQueue.erase(cursock);
+								continue;
+							}
+
+							if (cursock->second->netno >= 0) {
+								sq_stats.bytesSentIRC += toSend;
+							} else {
+								sq_stats.bytesSent += toSend;
+							}
+
+							buffer_remove_front(&cursock->second->buffer, toSend);
+
+#ifdef DEBUG
+							printf("SendQ[%p]: Sent %d bytes.\n", cursock->first, toSend);
+#endif
+						}
+					}
+					cursock++;
 				}
 			}
 
@@ -417,7 +281,6 @@ THREADTYPE SendQ_Thread(void * lpData) {
 
 	// these should be clear already
 	sqMutex.Lock();
-	buffers.clear();
 	PriorityQueue.clear();
 	sqMutex.Release();
 
